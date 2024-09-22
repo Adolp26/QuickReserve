e3b461d210a1b61649baa9adec07e98db62ae243
Adolphe Ribeiro <adolpheribeiro7@gmail.com>
2024-09-22 15:53:57 -0300

    database connection

--- src/server.js
+++ src/server.js
@@ -1,21 +1,19 @@
-require('dotenv').config(); // Carregar variáveis de ambiente
+require('dotenv').config();
 const express = require('express');
-const { testConnection } = require('./config/db'); // Ajuste o caminho conforme necessário
+const { testConnection } = require('./config/db'); 
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Defina uma porta, padrão é 3000
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
     // Testa a conexão com o banco de dados
     await testConnection();
     
-    // Se a conexão for bem-sucedida, inicie o servidor
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
     });
   } catch (err) {
-    // Se houver um erro de conexão, exiba o erro e encerre o processo
     console.error('Não foi possível iniciar o servidor:', err.message);
     process.exit(1); // Encerra o processo com um código de erro
   }
